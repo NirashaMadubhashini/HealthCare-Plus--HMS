6b4b7f60781ac75b65e53b1a1a24cda8f8190237
NirashaMadubhashini <nirashamadu0729@gmail.com>
2023-09-14 14:50:45 +0530

    update

--- HealthCare Plus- HMS/HospitalStaff/DoctorAvailability.cs
+++ HealthCare Plus- HMS/HospitalStaff/DoctorAvailability.cs
@@ -18,33 +18,32 @@ namespace HealthCare_Plus__HMS.HospitalStaff
         public DoctorAvailability()
         {
             InitializeComponent();
-            LoadDoctorIds();
-            LoadDoctorAvailability(); // Ensure data is loaded when the form is opened
+
 
             startTimeCb.Format = DateTimePickerFormat.Time;
             startTimeCb.ShowUpDown = true;
             endTimeCb.Format = DateTimePickerFormat.Time;
             endTimeCb.ShowUpDown = true;
 
-            docAvailableDGV.DataBindingComplete += new DataGridViewBindingCompleteEventHandler(docAvailableDGV_DataBindingComplete);
-            docAvailableDGV.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            LoadDoctorIDs();
 
         }
 
-
-
-        private void LoadDoctorIds()
+        private void LoadDoctorIDs()
         {
             try
             {
                 Con.Open();
                 SqlCommand cmd = new SqlCommand("SELECT doctor_id FROM DoctorTbl", Con);
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                docIdCb.Items.Clear();
+                while (dr.Read())
                 {
-                    docIdCb.Items.Add(reader.GetInt32(0));
+                    docIdCb.Items.Add(dr["doctor_id"].ToString());
                 }
-                reader.Close();
+
+                dr.Close();
             }
             catch (Exception ex)
             {
@@ -55,119 +54,17 @@ namespace HealthCare_Plus__HMS.HospitalStaff
                 Con.Close();
             }
         }
-
-        private void LoadDoctorAvailability()
-        {
-            try
-            {
-                if (Con.State == ConnectionState.Open)
-                {
-                    Con.Close();
-                }
-
-                Con.Open();
-                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM DoctorAvailabileTbl", Con);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-                docAvailableDGV.DataSource = dt;
-
-                // Make the table data fit to the table size
-                docAvailableDGV.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            }
-            catch (SqlException sqlEx)
-            {
-                MessageBox.Show("SQL Error: " + sqlEx.Message);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("General Error: " + ex.Message);
-            }
-            finally
-            {
-                Con.Close();
-            }
-        }
-
-
-
-        private bool ValidateInputs()
-        {
-            if (docIdCb.SelectedItem == null)
-            {
-                MessageBox.Show("Please select a doctor ID.");
-                return false;
-            }
-
-            // Check if at least one day is selected
-            if (!mondayCheckBox.Checked &&
-                !tuesdayCheckBox.Checked &&
-                !wednesdayCheckBox.Checked &&
-                !thursdayCheckBox.Checked &&
-                !fridayCheckBox.Checked &&
-                !saturdayCheckBox.Checked &&
-                !sundayCheckBox.Checked)
-            {
-                MessageBox.Show("Please select at least one day.");
-                return false;
-            }
-
-            if (startTimeCb.Value >= endTimeCb.Value)
-            {
-                MessageBox.Show("Start time must be earlier than end time.");
-                return false;
-            }
-
-            return true;
-        }
-
 
         private void addBtn_Click(object sender, EventArgs e)
         {
-            if (!ValidateInputs())
-                return;
 
-            try
-            {
-                Con.Open();
-
-                List<string> checkedDays = new List<string>();
-
-                if (mondayCheckBox.Checked) checkedDays.Add("Monday");
-                if (tuesdayCheckBox.Checked) checkedDays.Add("Tuesday");
-                if (wednesdayCheckBox.Checked) checkedDays.Add("Wednesday");
-                if (thursdayCheckBox.Checked) checkedDays.Add("Thursday");
-                if (fridayCheckBox.Checked) checkedDays.Add("Friday");
-                if (saturdayCheckBox.Checked) checkedDays.Add("Saturday");
-                if (sundayCheckBox.Checked) checkedDays.Add("Sunday");
-
-                foreach (string day in checkedDays)
-                {
-                    SqlCommand cmd = new SqlCommand("INSERT INTO DoctorAvailabileTbl (doctor_id, weekDays, availabilityStartTime, availabilityEndTime, doctorIsVacation) VALUES (@doctorId, @weekDays, @availabilityStartTime, @availabilityEndTime, @doctorIsVacation)", Con);
-                    cmd.Parameters.AddWithValue("@doctorId", docIdCb.SelectedItem);
-                    cmd.Parameters.AddWithValue("@weekDays", day);
-                    cmd.Parameters.AddWithValue("@availabilityStartTime", startTimeCb.Value.ToString("HH:mm:ss"));
-                    cmd.Parameters.AddWithValue("@availabilityEndTime", endTimeCb.Value.ToString("HH:mm:ss"));
-                    cmd.Parameters.AddWithValue("@doctorIsVacation", vacationCheckBox.Checked ? 1 : 0);
-
-                    cmd.ExecuteNonQuery();
-                }
-                LoadDoctorAvailability();
-
-                MessageBox.Show("Doctor availability added successfully");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                Con.Close();
-            }
         }
+
 
 
         private void updateBtn_Click(object sender, EventArgs e)
         {
+
 
         }
 
@@ -175,43 +72,16 @@ namespace HealthCare_Plus__HMS.HospitalStaff
         {
 
         }
-
-        private void docAvailableDGV_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
-        {
-            foreach (DataGridViewColumn column in docAvailableDGV.Columns)
-            {
-                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
-            }
-        }
         private void docAvailableDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+            
         }
+
 
         private void docIdCb_SelectedIndexChanged(object sender, EventArgs e)
         {
-            try
-            {
-                Con.Open();
-                SqlCommand cmd = new SqlCommand("SELECT D.doctorSpecialization, D.doctorQualifications, D.roomNumber, U.userName FROM DoctorTbl D INNER JOIN UserTbl U ON D.doctor_id = U.user_id WHERE D.doctor_id = @doctorId", Con);
-                cmd.Parameters.AddWithValue("@doctorId", docIdCb.SelectedItem);
-                SqlDataReader reader = cmd.ExecuteReader();
-                if (reader.Read())
-                {
-                    docNameTb.Text = reader.GetString(3);
-                    docSpecTb.Text = reader.GetString(0);
-                    docRoomTb.Text = reader.GetString(2);
-                }
-                reader.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                Con.Close();
-            }
+
         }
 
 
